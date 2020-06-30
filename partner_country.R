08317cc2d43a52f233025883b1c13272e1a28acf
ambijat <44221226+ambijat@users.noreply.github.com>
2020-06-30 21:27:55 +0530

    Update partner_country.R

--- partner_country.R
+++ partner_country.R
@@ -1,27 +1,28 @@
 setwd("D:/R5b")
 '''
-this script is to filter the list of country based on ther GDP for defining the variables for
-panel data.
+this script is to filter the list of country based on ther GDP for defining the variables for panel data.
 '''
+library(dplyr)
+library(tidyverse)
+
 library(WDI)
-
+#searching for Gross National Income (GNI)
 WDIsearch("gni")
-library(readxl)
-country_list <- read_excel("comtradecountry_nowlist.xlsx")
-colnames(country_list) <- c("country.code", "country.name", "iso3c")
-
 #this indicator is for GNI ar current USD.
 indicator <- "NY.GNP.MKTP.CD"
 wdi_cat <- WDI(indicator = indicator, start = 2010, end = 2010, extra = T)
 names(wdi_cat)
-
-library(dplyr)
-library(tidyverse)
-
 wdi2 <- wdi_cat %>%
   select("country", "NY.GNP.MKTP.CD", "iso3c")
 
-#joining the list
+#loading country_list from excel file downloaded from
+#https://unstats.un.org/unsd/tradekb/Knowledgebase/50377/Comtrade-Country-Code-and-Name
+library(readxl)
+country_list <- read_excel("comtradecountry_nowlist.xlsx")
+#renaming the columns
+colnames(country_list) <- c("country.code", "country.name", "iso3c")
+
+#joining the 2 dataframes
 pp <- left_join(country_list, wdi2, by = "iso3c")
 colnames(pp)[5] <- "GNI"
 names(pp)
